297f063f8492fae53649989443402004a0e4be01
rm93897 <rm93897@fiap.com.br>
2023-08-27 17:01:31 -0300

    Botão e Lista Suspensa

--- src/app/CadastroAluno/page.js
+++ src/app/CadastroAluno/page.js
@@ -1,42 +1,49 @@
 
+import DropMenu from "@/components/DropMenu";
 import NavBar from "@/components/NavBar";
-
+import { CheckIcon } from '@heroicons/react/24/solid'
+import { Button, Checkbox } from "@mui/material";
 
 export default function CadastroAluno() {
   return (
+    <>
+      <NavBar />
 
-<>
-  <NavBar/>
-
-    <main className="bg-slate-300 m-20 p-8">
+      <main className="bg-slate-300 m-20 p-8 rounded">
       <h2 className="text-xl flex justify-center">Cadastro Aluno</h2>
 
      <div>
       <div id="data">
        <div id="data-row">
-        <div>
-          <span>Nome</span> 
+        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
+          <span>Nome:</span>
+          <DropMenu />
         </div>
-        <div>
-          <span>E-mail</span> 
+
+        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
+          <span>E-mail:</span> 
+          <DropMenu />
         </div>
-        <div>
+
+        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
           <span>Senha</span>
-          </div>
-          <div>
           <span>Confirmação de Senha</span>
           </div>
-          <div>
+
+          <div className="hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
           <span>Instituição</span>
-          </div> 
-          <div>
-          <span>i</span>
-          <span>Proferssor</span>
-          <span>i</span>
-          <span>Aluno</span>
-          <span>i</span>
-          <span>Criar Cadastro</span>
-          </div> 
+          </div>
+
+          <div className="flex gap-10 hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
+          <Checkbox/>
+          <span >Proferssor</span>
+          <Checkbox/>
+             <span>Aluno</span>
+          </div>
+
+          <div className="flex justify-end focus:bg-slate-200 p-2 my-4 cursor-pointer rounded  ">
+          <Button />
+          </div>
        </div>
       </div>
      </div>
--- src/app/CadastroProfessor/page.js
+++ src/app/CadastroProfessor/page.js
@@ -1,4 +1,7 @@
+import DropMenu from "@/components/DropMenu";
 import NavBar from "@/components/NavBar";
+import { CheckIcon } from '@heroicons/react/24/solid'
+import { Button, Checkbox } from "@mui/material";
 
 export default function CadastroProfessor() {
   return (
@@ -11,32 +14,34 @@ export default function CadastroProfessor() {
      <div>
       <div id="data">
        <div id="data-row">
-        <div className="hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
-          <span>Nome:</span> 
+        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
+          <span>Nome:</span>
+          <DropMenu />
         </div>
-        <div className="hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
+
+        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
           <span>E-mail:</span> 
+          <DropMenu />
         </div>
+
         <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
           <span>Senha</span>
           <span>Confirmação de Senha</span>
           </div>
+
           <div className="hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
           <span>Instituição</span>
-          </div> 
-          <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-          <span >Proferssor</span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-
-          <span>Aluno</span>
           </div>
-          <div className="flex justify-end hover:bg-slate-200 p-2 my-4 cursor-pointer rounded  ">
-          <span>Criar Cadastro</span>
+
+          <div className="flex gap-10 hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
+          <Checkbox/>
+          <span >Proferssor</span>
+          <Checkbox/>
+             <span>Aluno</span>
+          </div>
+
+          <div className="flex justify-end focus:bg-slate-200 p-2 my-4 cursor-pointer rounded  ">
+          <Button />
           </div>
        </div>
       </div>
