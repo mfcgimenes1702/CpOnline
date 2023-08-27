12b7a96564c10983a9d20c2d716f784f0e7f28ba
rm93897 <rm93897@fiap.com.br>
2023-08-27 18:15:40 -0300

    Data Fetching

--- src/app/CadastroAluno/page.js
+++ src/app/CadastroAluno/page.js
@@ -1,50 +1,27 @@
-
-import DropMenu from "@/components/DropMenu";
 import NavBar from "@/components/NavBar";
-import { CheckIcon } from '@heroicons/react/24/solid'
-import { Button, Checkbox } from "@mui/material";
+import DataRow from "./DataRow";
 
-export default function CadastroAluno() {
+async function getCadastro(){
+  const url ="http://localhost:8080/api/aluno"
+  const response = await fetch(url, {next:{ revalidate: 0}})
+  return response.json()
+}
+
+export default async function CadastroAluno() {
+  const data = await getCadastro()
+    
   return (
     <>
-      <NavBar />
+      <NavBar active={"Cadastro"}/>
 
       <main className="bg-slate-300 m-20 p-8 rounded">
       <h2 className="text-xl flex justify-center">Cadastro Aluno</h2>
 
      <div>
       <div id="data">
-       <div id="data-row">
-        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
-          <span>Nome:</span>
-          <DropMenu />
-        </div>
-
-        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
-          <span>E-mail:</span> 
-          <DropMenu />
-        </div>
-
-        <div className="flex justify-between hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
-          <span>Senha</span>
-          <span>Confirmação de Senha</span>
-          </div>
-
-          <div className="hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
-          <span>Instituição</span>
-          </div>
-
-          <div className="flex gap-10 hover:bg-slate-200 p-2 my-4 cursor-pointer rounded">
-          <Checkbox/>
-          <span >Proferssor</span>
-          <Checkbox/>
-             <span>Aluno</span>
-          </div>
-
-          <div className="flex justify-end focus:bg-slate-200 p-2 my-4 cursor-pointer rounded  ">
-          <Button />
-          </div>
-       </div>
+       {data.map(Cadastro =>{
+        return <DataRow Cadastro={Cadastro}/>
+       })}
       </div>
      </div>
     </main>
